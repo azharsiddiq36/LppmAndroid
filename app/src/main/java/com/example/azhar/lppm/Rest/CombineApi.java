b9398634a885142d64bd3d6c8cb6ddd2a4407408
Azhar <azhar.siddiq@students.uin-suska.ac.id>
2019-06-12 17:37:25 +0700

    Update CombineApi.java

--- app/src/main/java/com/example/azhar/lppm/Rest/CombineApi.java
+++ app/src/main/java/com/example/azhar/lppm/Rest/CombineApi.java
@@ -2,9 +2,8 @@ package com.example.azhar.lppm.Rest;
 
 public class CombineApi {
 
-    public static final String BASE_URL = "http://karir-suska.org/lppm/lppm/office/";
-    //public static final String BASE_URL = "http://192.168.43.201/lppm/office/";
-    //public static final String BASE_URL = "http://10.0.0.2/lppm/office/";
+    public static final String BASE_URL = "YOUR_IP";
+
     public static final String BASE_URL_PROV = "https://dev.farizdotid.com/api/daerahindonesia/";
     public static LppmInterface getApiService(){
         return ApiClient.getApiClient(BASE_URL).create(LppmInterface.class);
